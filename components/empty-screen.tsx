31dc204b162064c057e885665fc52808640e22f3
fibleep <filipn924@gmail.com>
2023-12-08 14:38:54 +0100

    display current location

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -3,6 +3,7 @@ import { UseChatHelpers } from 'ai/react'
 import { Button } from '@/components/ui/button'
 import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
+import { useEffect, useState } from 'react'
 
 const exampleMessages = [
   {
@@ -20,6 +21,38 @@ const exampleMessages = [
 ]
 
 export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
+   const [location, setLocation] = useState({ 
+    latitude: null, 
+    longitude: null, 
+    altitude: null 
+  });
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    let watchId;
+
+    if ('geolocation' in navigator) {
+      watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            altitude: position.coords.altitude
+          });
+        },
+        (error) => {
+          setError(error.message);
+        }
+      );
+    } else {
+      setError('Geolocation is not supported by your browser.');
+    }
+
+    return () => {
+      if (watchId) navigator.geolocation.clearWatch(watchId);
+    };
+  }, []);
+  
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
@@ -44,6 +77,12 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
               {message.heading}
             </Button>
           ))}
+          <p className="text-sm text-muted-foreground">
+            Latitude: {location.latitude} <br/>
+            Longitude: {location.longitude} <br/>
+            {location.altitude !== null ? `Altitude: ${location.altitude} meters` : ''}
+
+          </p>
         </div>
       </div>
     </div>
